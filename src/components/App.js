9d90b3b4210fc9cbd808734de881be9baf27e6ab
Shannon Grantski <grantski@pm.me>
2021-02-09 11:43:52 -0800

    add base structure to App.js
    Co-authored-by: Ryland Adams <rylandadams@yahoo.com>
    Co-authored-by: Tawnee Harris <tawneeh@icloud.com>

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,18 @@
 import React from "react";
+import Header from "./Header";
+import PlanVisit from "./PlanVisit";
+import About from "./About";
+import NewContactForm from "./NewContactForm";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <PlanVisit />
+      <About />
+      <NewContactForm />
+    </>
+  );
+}
+
+export default App;
--- src/components/PlanVist.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-
-// export default PlanVisit;
-
-// DayLocation and MonthProduce will be children of PlanVisit
