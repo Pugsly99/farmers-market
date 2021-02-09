22a23217db86a2b2122e3870e7549065af85c8a5
Shannon Grantski <grantski@pm.me>
2021-02-09 13:15:19 -0800

    insert import functions
    Co-authored-by: Ryland Adams <rylandadams@yahoo.com>
    Co-authored-by: Tawnee Harris <tawneeh@icloud.com>

--- src/components/PlanVisit.js
+++ src/components/PlanVisit.js
@@ -1,4 +1,14 @@
 import React from "react";
+import DayLocation from "./DayLocation";
+import MonthProduce from "./MonthProduce";
 
+function PlanVisit() {
+  return (
+    <>
+      <DayLocation />
+      <MonthProduce />
+    </>
+  );
+}
 
-export default PlanVist;
+export default PlanVisit;
