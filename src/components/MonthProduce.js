fba768bdf6593f630ce4eebe737201569bb98182
Shannon Grantski <grantski@pm.me>
2021-02-09 13:33:13 -0800

    add values for monthproduce
    Co-authored-by: Ryland Adams <rylandadams@yahoo.com>
    Co-authored-by: Tawnee Harris <tawneeh@icloud.com>

--- src/components/MonthProduce.js
+++ src/components/MonthProduce.js
@@ -1,10 +1,21 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function MonthProduce(){
+function MonthProduce(props){
   return(
-    <h1> placeholder for MonthProduce </h1>
+    <>
+    <h3>{props.month}</h3>
+    <h3>{props.selection}</h3>
+    <hr />
+  </>
   );
 }
+
+MonthProduce.propTypes = {
+  day: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+};
+
 
 export default MonthProduce;
 
