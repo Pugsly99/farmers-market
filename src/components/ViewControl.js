6459dc7f018a603d7bf3777f3dc6c232a204e673
Shannon Grantski <grantski@pm.me>
2021-02-09 16:06:15 -0800

    remove comments and unused code
    Co-authored-by: Ryland Adams <rylandadams@yahoo.com>
    Co-authored-by: Tawnee Harris <tawneeh@icloud.com>

--- src/components/ViewControl.js
+++ src/components/ViewControl.js
@@ -1,8 +1,7 @@
 import React from "react";
 import PlanVisit from './PlanVisit';
-import About from "./About";
-import NewContactForm from "./NewContactForm";
-// import Header from "./Header";
+import About from './About';
+import NewContactForm from './NewContactForm';
 
 class ViewControl extends React.Component {
 
@@ -12,10 +11,7 @@ class ViewControl extends React.Component {
       visibleView: 0
     };
   }
-  // 0 = page with header only
-  // 1 = plan visit
-  // 2 = about
-  // 3 = contact
+  
 
   planClick = () => {
     this.setState({
@@ -39,9 +35,8 @@ class ViewControl extends React.Component {
 
   render(){
       let currentlyVisibleState = null;
-      let buttonText = null;
       if (this.state.visibleView === 0) {
-        currentlyVisibleState = null // this might have to be something like "null" ;; Header's visibility shouldn't be toggled on and off
+        currentlyVisibleState = null
       } else if(this.state.visibleView === 1) {
         currentlyVisibleState = <PlanVisit />
       } else if(this.state.visibleView === 2) {
@@ -51,7 +46,6 @@ class ViewControl extends React.Component {
       }
       return (
         <>
-        <h1>view control placeholder heading (make sure it renders) </h1>
           <button onClick={this.planClick}> Plan Your Visit </button>
           <button onClick={this.aboutClick}> About </button>
           <button onClick={this.contactClick}> Contact </button>
